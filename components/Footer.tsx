fc3d16d07566ba754cf7922a951a26e03eee9217
MT XA <qbuiminh5@gmail.com>
2026-01-15 22:10:43 +0700

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -50,7 +50,7 @@ export function Footer() {
             </Link>
           </div>
           
-          <div className="w-full h-48 bg-white/10 rounded overflow-hidden">
+          {/* <div className="w-full h-48 bg-white/10 rounded overflow-hidden">
             <iframe 
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.588582210996!2d105.8049103!3d21.0091219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac978f86f77f%3A0x633919ad778f657!2zMiBOZy4gNjggUC4gUXVhbiBOaMOibiwgVHJ1bmcgSG_DoCwgQ-G6p3UgR2nhuqV5LCBIwG4gTuG7mWksIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1736915000000!5m2!1sen!2s" 
               width="100%" 
@@ -60,7 +60,7 @@ export function Footer() {
               loading="lazy" 
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
