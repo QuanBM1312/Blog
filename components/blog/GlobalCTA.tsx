9155bf606a90bc50a665e473599f4f855eaec2cf
MSI <quiminh5@gmail.com>
2026-02-23 22:39:59 +0700

    update

--- components/blog/GlobalCTA.tsx
+++ components/blog/GlobalCTA.tsx
@@ -7,7 +7,8 @@ export function GlobalCTA() {
         "Mời bạn ghé Hải Lĩnh Y Quán dùng một tách trà ấm và hàn huyên đôi câu chuyện sức khỏe cùng chúng tôi. Cánh cửa nơi đây luôn rộng mở chào đón bạn!"
       </p>
       <Link 
-        href="/contact"
+        href="https://www.facebook.com/profile.php?id=61580977185742"
+        target="_blank"
         className="inline-block bg-primary text-white font-montserrat font-bold py-4 px-10 uppercase tracking-widest hover:bg-accent transition-colors duration-300"
       >
         Liên Hệ Ngay
